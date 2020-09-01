a9cdaddbfa1528127981654d18bf73ce78ea41dd
Pom Nawapat <pomm-land@hotmail.co.th>
2020-09-02 00:57:27 +0700

    Add Another page, Edite views and urls

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from product.views import home_view
+from product.views import home_view,contact_view,about_view
 
 urlpatterns = [
     path('', home_view, name ="home"),
+    path('about/', contact_view),
+    path('contact/', about_view),
     path('admin/', admin.site.urls),
 ]
