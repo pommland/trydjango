bd2f2d113078c3cc46d21688a7bdd28a1c257e57
pommland <54527556+pommland@users.noreply.github.com>
2020-09-10 01:45:57 +0700

    Form widgets

--- src/product/forms.py
+++ src/product/forms.py
@@ -15,6 +15,16 @@ class ProductForm(forms.ModelForm):
 
 # https://docs.djangoproject.com/en/3.1/ref/forms/fields/,
 class RawProductForm(forms.Form):
-    title       = forms.CharField()
-    description = forms.CharField()
-    price       = forms.DecimalField() 
+    title       = forms.CharField(label = "",widget = forms.TextInput(attrs={"placeholder" : "Your title"}))
+    description = forms.CharField(required = False ,
+                                  widget = forms.Textarea(
+                                      attrs = {
+                                          "placeholder" : "Your title",
+                                          "class" : "new class name two",
+                                          "id" : "my_id_pom",
+                                          "rows" : 20,
+                                          "cols" : 80
+                                      }
+                                    )
+                                )
+    price       = forms.DecimalField(initial = 199.99) 
