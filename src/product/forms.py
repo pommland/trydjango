e60ad8e2c76665930fb003f64ae7570c1d79d555
pommland <54527556+pommland@users.noreply.github.com>
2020-09-11 18:38:19 +0700

    edite Form Validation Methods

--- src/product/forms.py
+++ src/product/forms.py
@@ -4,6 +4,23 @@ from .models import  Product
 
 
 class ProductForm(forms.ModelForm):
+
+    title       = forms.CharField(label = "",
+                        widget = forms.TextInput(attrs={"placeholder" : "Your title"}))
+    email       = forms.EmailField()
+    description = forms.CharField(required = False ,
+                                widget = forms.Textarea(
+                                    attrs = {
+                                        "placeholder" : "Your title",
+                                        "class" : "new class name two",
+                                        "id" : "my_id_pom",
+                                        "rows" : 20,
+                                        "cols" : 80
+                                    }
+                                )
+                            )
+    price       = forms.DecimalField(initial = 199.99) 
+
     class Meta:
         model = Product
         fields = [
@@ -11,7 +28,17 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
-
+    def clean_title(self, *args,**kwargs):
+        title = self.cleaned_data.get('title')
+        if not "CFE" in title :
+            raise forms.ValidationError("This is not Valid title")
+        return title
+    
+    def clean_email(self, *args , **kwargs):
+        email = self.cleaned_data.get("email")
+        if not email.endswith("edu"):
+            raise forms.ValidationError("This is not valid email")
+        return email
 
 # https://docs.djangoproject.com/en/3.1/ref/forms/fields/,
 class RawProductForm(forms.Form):
